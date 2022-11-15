2c2049f89c74bb76339d25dd6151fdbd3677b6c4
turcualexd <turcualex01@gmail.com>
2022-11-15 19:24:04 +0100

    Sistemato directTransfer e creato file di prova

--- directTransfer.m
+++ directTransfer.m
@@ -1,22 +1,24 @@
 function [a, e, i, OM, om] = directTransfer(rri, rrf)
 
+% calcolo il versore normale al piano orbitale (lo prendo direzionato come k)
 n = cross(rri, rrf) / norm(cross(rri, rrf));
 if n(3) < 0
     n = -n;
 end
 
+% inclinazione (da 0 a pi)
 i = acos(n(3));
 
+% asse dei nodi
 N = cross([0 0 1]', n);
 
+% RAAN
 OM = acos(N(1));
 if N(2) < 0
     OM = 2*pi - OM;
 end
 
-[ai, ei, i_i, OMi, omi, thi] = car2par(rri, vvi, 'rad')
-[af, ef, i_f, OMf, omf, thf] = car2par(rrf, vvf, 'rad')
-
+% trovo raggio minore e maggiore
 if norm(rri) <= norm(rrf)
     rmin = rri;
     rmax = rrf;
@@ -25,15 +27,19 @@ else
     rmax = rri;
 end
 
-% Pongo rmin come perigeo
+% N.B.: come scelta per orbita pongo rmin come perigeo (ma si può cambiare)
 
+% angolo del perigeo rispetto a N
 om = acos( dot(N, rmin) / norm(rmin) );
 if rmin(3) < 0
     om = 2*pi - om;
 end
 
+% trovo il coseno dell'angolo di arrivo rispetto a quello di partenza
 cos_thf = dot(rri, rrf) / (norm(rri) * norm(rrf));
 
+% eccentricità
 e = (norm(rmax) - norm(rmin)) / (norm(rmin) - cos_thf * norm(rmax));
 
+% semiasse maggiore
 a = norm(rmin) / (1 - e);
