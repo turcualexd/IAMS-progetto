0548af6b1177fa73f5b841930c7b3f915c4d1136
Paocrazia <115695590+Paocrazia@users.noreply.github.com>
2022-11-29 00:38:38 +0100

    minimizzo il deltav con 3 impulsi
    ho trovato una cosa interessante, devo mettere un altro for e scriverla meglio dottore mi serve il suo aiuto

--- strategia_paolo_05.m
+++ strategia_paolo_05.m
@@ -1,4 +1,4 @@
-clear; clc; close all
+clear; close all
 
 %% dati iniziali
 rr = [-1169.7791 -8344.5289 977.8062]';
@@ -16,31 +16,51 @@ thf = 0.3316;
 [rrf, vvf] = par2car(af, ef, i_f, OMf, omf, thf);
 [rrfp, vvfp] = par2car(af, ef, i_f, OMf, omf, 0);
 
-deltav = 0;
-deltat = 0;
 
-%% 1: passo su nuova orbita circolare di raggio ra
+vel = [];
 
-raf = af * ( 1 + ef ); % raggio orbita dopo bitangente
-ec = 0;
+% faccio fondere il pc
 
-deltat1 = TOF(ai, ei, thi, 2*pi); % arrivo al punto di trasferimento
+% ee = 0:1e-1:1;
+aa = ai:1e2:af;
+aaa = aa(10):1e1:aa(12);
+aaaa = aaa(2):1:aaa(4);
+aaaaa = aaaa(8):1e-1:aaaa(10);
+valore_ottimale = aaaaa(11);
+minimo = [1e10 0 0 0 0];
 
-[DeltaV1, DeltaV2, Deltat, om_f_new, om_t, a_t, e_t] = bitangentTransfer(ai, ei, raf, ec, 'pa' , omi);
+for j = 1:1
 
-deltat2 = TOF(a_t, e_t, 0, pi);
+    deltav = 0;
+    deltat = 0;
+    raf = aaaaa(11);
 
-deltav = deltav + abs(DeltaV1) + abs(DeltaV2);
-deltat = deltat + deltat1 + deltat2;
+    ec = 0;
+    deltat1 = TOF(ai, ei, thi, 2*pi); % arrivo al punto di trasferimento
+    
+    [DeltaV1, DeltaV2, Deltat, om_f_new, om_t, a_t, e_t] = bitangentTransfer(ai, ei, raf, ec, 'pa' , omi);
+    
+    deltat2 = TOF(a_t, e_t, 0, pi); % questo va messo a posto, non arrivo a pi ma al punto definito dopo
+    
+    deltav = deltav + abs(DeltaV1);
+    deltat = deltat + deltat1;
+    
+    %% cerco secante ottimale per arrivare sull'orbita finale
+    
+    [rr1, vv1] = par2car(a_t, e_t, ii, OMi, om_t, 0); % punto di arrivo dell'orbita bitangente
+    
+    [at_s, et_s, i_t_s, OMt_s, omt_s, th1_trasf, th2_trasf, th1_iniziale, th2_finale, deltaV_tot, deltaT_tot, rr1_s, rr2_s] = secante_ottimale(rr1, vv1, rrf, vvf, nan, nan, 1);
+    
+    deltav = deltav + deltaV_tot;
+    deltat = deltat + deltaT_tot;
 
-%% cerco secante ottimale per arrivare sull'orbita finale
+    vel = [vel; deltav]
 
-[rr1, vv1] = par2car(a_t, e_t, ii, OMi, om_t, 0); % punto di arrivo dell'orbita bitangente
-
-[at_s, et_s, i_t_s, OMt_s, omt_s, th1_trasf, th2_trasf, th1_iniziale, th2_finale, deltaV_tot, deltaT_tot, rr1_s, rr2_s] = secante_ottimale(rr1, vv1, rrf, vvf, nan, nan, true);
-
-deltav = deltav + deltaV_tot;
-deltat = deltat + deltaT_tot;
+    if deltav < minimo(1,1)
+        minimo = [deltav aa(j) j];
+    end
+   
+end
 %% plot
 
 %Terra_3D
