85f0c8a758d0e25f7762e9219b9b1f24dafa2889
Paocrazia <115695590+Paocrazia@users.noreply.github.com>
2022-12-24 19:58:34 +0100

    presentazione

--- aaa_dati_iniziali.m
+++ aaa_dati_iniziali.m
@@ -30,6 +30,26 @@ legend('', 'Initial Orbit', 'Final Orbit',...
 Terra_3D
 
 plot3(rri(1), rri(2), rri(3), 'ro', LineWidth=1);
+
 quiver3(rri(1), rri(2), rri(3), vvi(1), vvi(2), vvi(3), 1e3, 'r', LineWidth=1);
 plotOrbit_leggero(ai, ei, i_i, OMi, omi, 0, 2*pi, 0.001, 'rad', 'r--');
+
 legend('', 'Initial position', 'Initial velocity','Found Orbit', fontsize=15)
+
+%%
+
+Terra_3D
+
+plotOrbit(af, ef, i_f, OMf, omf, 0, 2*pi, 0.001, 'rad', 'b');
+
+quiver3(0,0,0, rrf(1), rrf(2), rrf(3), 'b', LineWidth=1,ShowArrowHead='on');
+
+plot3(rrf(1), rrf(2), rrf(3), 'co', LineWidth=2);
+
+quiver3(rrf(1), rrf(2), rrf(3), vvf(1), vvf(2), vvf(3),1e3,...
+    'c', LineWidth=1,ShowArrowHead='on');
+
+legend('', 'Initial Orbit', 'Initial position','Found point', 'Found velocity', fontsize=15)
+
+
+
