00e40f9d4f8e6b21599692f469a86e6dd1f1e7dd
Paocrazia <115695590+Paocrazia@users.noreply.github.com>
2022-12-27 11:14:21 +0100

    Update grafici_conclusioni.m

--- grafici_conclusioni.m
+++ grafici_conclusioni.m
@@ -4,8 +4,8 @@ close all
 
 tempi = [17523.1496 14461.7429 17464.4130 21973.3098 18728.5707 28868.3598 15603.0824 14421.7183 32409.9559 8964.9024 28259.7957];
 vv = [6.6450 7.1386 7.1222 6.6614 9.0993 9.1511 7.4603 7.4767 6.9762 5.1306 5.3574];
-forma = ['o'; 'o'; 'o'; 'o'; 'o'; 'o'; 's'; 's'; 's'; 's'; 's'];
-spessore = 4;
+forma = ['d'; 'o'; 'o'; 'd'; 'o'; 'o'; 'o'; 'o'; 's'; 'd'; 'd'];
+spessore = 4; 
 
 plot_paolo(tempi, vv, forma, spessore);
 
@@ -14,5 +14,5 @@ xlabel('Time required [s]');
 vv_min = min(vv);
 t_min = min(tempi);
 
-legend('S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'A1', 'A2', 'A3', fontsize=15); 
-
+legend('S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'A1', ...
+    'A2', 'A3', fontsize=15); 
