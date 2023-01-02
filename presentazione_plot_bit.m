8fd68b251fab363a72c94c793689f10ee905dbfb
Silvietta25 <115651216+Silvietta25@users.noreply.github.com>
2023-01-02 21:45:51 +0100

    presentazione

--- presentazione_plot_bit.m
+++ presentazione_plot_bit.m
@@ -60,20 +60,20 @@ deltat_tot_h = deltat_tot/3600 %tempo in ore
 
 % iniziale
 Terra_3D
-plotOrbit_leggero(ai, ei, ii, OMi, omi, 0, 2*pi, 0.001, 'rad', 'r--')
-plotOrbit(ai, ei, ii, OMi, omi, thi, 2*pi, 0.001, 'rad', 'r')
+%plotOrbit_leggero(ai, ei, ii, OMi, omi, 0, 2*pi, 0.001, 'rad', 'r--')
+plotOrbit_leggero(ai, ei, ii, OMi, omi, thi, 2*pi, 0.001, 'rad', 'r')
 hold on
 % arco
 plotOrbit(at, et, ii, OMi, omt, 0, pi, 0.001, 'rad', 'm')
-plotOrbit(af, ef, ii, OMi, omt, pi, theta_cp, 0.001, 'rad', 'c')
+plotOrbit_leggero(af, ef, ii, OMi, omt, pi, theta_cp, 0.001, 'rad', 'c--')
 
 % cambio piano
-plotOrbit(af, ef, i_f, OMf, om2, theta_cp,theta_cwi , 0.001, 'rad', 'g')
+plotOrbit_leggero(af, ef, i_f, OMf, om2, theta_cp,theta_cwi , 0.001, 'rad', 'g--')
 
 
 % finale
-plotOrbit_leggero(af, ef, i_f, OMf, omf, 0, 2*pi, 0.001, 'rad', 'b--')
-plotOrbit(af, ef, i_f, OMf, omf, theta_cwf, thf, 0.001, 'rad', 'b')
+%plotOrbit_leggero(af, ef, i_f, OMf, omf, 0, 2*pi, 0.001, 'rad', 'b--')
+plotOrbit_leggero(af, ef, i_f, OMf, omf, theta_cwf, thf, 0.001, 'rad', 'b--')
 
 
 
@@ -83,24 +83,25 @@ plot3(rr(1), rr(2), rr(3), 'ks', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceC
 
 [rrpt, vvpt] = par2car(at, et, ii, OMi, omt, 0, "rad");
 plot3(rrpt(1), rrpt(2), rrpt(3), 'ko', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='m');
+
 [rrat, vvat] = par2car(at, et, ii, OMi, omt, pi, "rad");
 plot3(rrat(1), rrat(2), rrat(3), 'ko', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='c');
 
-[rrcp, vvcp] = par2car(af, ef, i_f, OMf, om2, theta_cp, "rad");
-plot3(rrcp(1), rrcp(2), rrcp(3), 'ko', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='g');
+% [rrcp, vvcp] = par2car(af, ef, i_f, OMf, om2, theta_cp, "rad");
+% plot3(rrcp(1), rrcp(2), rrcp(3), 'ko', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='g');
+% 
+% [rrcw, vvcw] = par2car(af, ef, i_f, OMf, omf, theta_cwf, "rad");
+% plot3(rrcw(1), rrcw(2), rrcw(3), 'ko', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='b');
 
-[rrcw, vvcw] = par2car(af, ef, i_f, OMf, omf, theta_cwf, "rad");
-plot3(rrcw(1), rrcw(2), rrcw(3), 'ko', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='b');
-
-[rr2, vv2] = par2car(af, ef, i_f, OMf, omf, thf, "rad");
-plot3(rr2(1), rr2(2), rr2(3), 'ks', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='b');
+% [rr2, vv2] = par2car(af, ef, i_f, OMf, omf, thf, "rad");
+% plot3(rr2(1), rr2(2), rr2(3), 'ks', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='b');
 
 
 % legenda
-legend('','', ...
+legend('',...
         'Initial Orbit', ...   %r
         'Bitangent Orbit',...    %m
         'Transfer Orbit', ...   %c
-        'Change Plane Orbit','', ...  % g
+        'Change Plane Orbit', ...  % g
         'Final Orbit', ...          %b
          fontsize=15)   %k*
