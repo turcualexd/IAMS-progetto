ed155045a0b89afeea8b23e0db39b9f554f3df07
turcualexd <turcualex01@gmail.com>
2023-01-09 22:29:49 +0100

    Squashed commit of the following:
    commit fd749803140fc921c13d95518583d49b03505c35
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Mon Jan 9 22:28:51 2023 +0100
    
        Update C13_presentazione.pptx
    
    commit 88445f3c5608ad40cda217d9412db2d77cc0539b
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Mon Jan 9 15:38:04 2023 +0100
    
        Update C13_presentazione.pptx
    
    commit b9fd9b9b6fa2d0bd4c1aa7bf43780ec31a05674a
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Mon Jan 9 15:12:38 2023 +0100
    
        Update C13_presentazione.pptx
    
    commit ccb7bad92098eb51b0f70874d6671fb4b6343a32
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Mon Jan 9 15:07:06 2023 +0100
    
        Update C13_presentazione.pptx
    
    commit eb24f95418ecb62a2231e96e6936e54f78dc4eaf
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Mon Jan 9 14:59:38 2023 +0100
    
        Update C13_presentazione.pptx
    
    commit 8003b93511790ff0fe69246d543e3f7ed75854be
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Mon Jan 9 13:15:46 2023 +0100
    
        Update C13_presentazione.pptx
    
    commit 39fd5e8bfb8651c8df57f86192a394ae86188fd5
    Author: Silvietta25 <115651216+Silvietta25@users.noreply.github.com>
    Date:   Sun Jan 8 20:30:06 2023 +0100
    
        Create C13_presentazione.pptx
    
    commit 9c11ec94e37420c43f92ebd1cbec3a2275d6b47a
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Sun Jan 8 20:13:17 2023 +0100
    
        Update Power C13_completo.pptx
    
    commit abd61ef79353e6a946d8650baddf27dd95a28ed8
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Sun Jan 8 17:39:02 2023 +0100
    
        Create Power C13_completo.pptx
    
    commit 4bda62f46a90c2d906da264caf43e9496042eb69
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Sat Jan 7 20:29:29 2023 +0100
    
        Finito testo relazione e sfondo
    
    commit 4f3c0c2b6f4ed02cef64333a0f2a0c354b709748
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Sat Jan 7 02:05:54 2023 +0100
    
        Prove sfondi
    
    commit d7083b2633c0b918b1d4e52b41c119cf8f3de86c
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Sat Jan 7 00:41:49 2023 +0100
    
        Volevo dire manovra secante ops
    
    commit 78a864d947b3538f4f96e426a2d38ea64ece52be
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Fri Jan 6 21:21:30 2023 +0100
    
        Conclusa presentazione tangente
    
    commit a2a808142ba3d55a8a0dccdf37a53c2b03fd576b
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Fri Jan 6 18:19:51 2023 +0100
    
        Update Power C13.pptx
    
    commit 99c9fd54a0b44fdea34a873b75e9c36e9ebadca9
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Thu Jan 5 00:01:37 2023 +0100
    
        Manovra tangente presentazione
    
    commit 3ca548d738d71c02b370eec96fe9d6bb79d20fd3
    Author: turcualexd <turcualex01@gmail.com>
    Date:   Wed Jan 4 19:17:56 2023 +0100
    
        Plot manovra magica

---
+++ alex_plot_deltaV_minimo.m
@@ -0,0 +1,35 @@
+clear, clc, close all;
+load("alex_deltaV_minimo_workspace.mat")
+
+Terra_3D
+
+% Orbita iniziale
+plotOrbit(ai, ei, i_i, OMi, omi, thi, th1_iniziale, 0.001, 'rad', 'r');
+plotOrbit_leggero(ai, ei, i_i, OMi, omi, th1_iniziale, thi, 0.001, 'rad', 'r--');
+
+% Orbita di trasferimento
+plotOrbit(at, et, i_t, OMt, omt, th1_trasf, th2_trasf, 0.001, 'rad', 'g');
+plotOrbit_leggero(at, et, i_t, OMt, omt, th2_trasf, th1_trasf, 0.001, 'rad', 'g--');
+
+% Orbita finale
+plotOrbit(af, ef, i_f, OMf, omf, th2_finale, thf, 0.001, 'rad', 'b');
+plotOrbit_leggero(af, ef, i_f, OMf, omf, thf, th2_finale, 0.001, 'rad', 'b--');
+
+% Punto iniziale
+plot3(rri(1), rri(2), rri(3), 'ks', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='r')
+
+% Punto di manovra 1
+plot3(rr1(1), rr1(2), rr1(3), 'ko', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='g')
+
+% Punto di manovra 2
+plot3(rr2(1), rr2(2), rr2(3), 'ko', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='b')
+
+% Punto finale
+plot3(rrf(1), rrf(2), rrf(3), 'ks', MarkerSize=10, MarkerEdgeColor='k', MarkerFaceColor='b')
+
+% Legenda
+legend('',...
+        'Initial Orbit', '', ...
+        'Transfer Orbit', '', ...
+        'Final Orbit', ...
+         fontsize=15)
